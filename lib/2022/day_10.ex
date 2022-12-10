8f5093ad307fda1b248c9554a58aafcedac1fb32
balysv <balys@valentukevicius.com>
2022-12-10 15:08:42 +0100

    2022, Day 10

--- lib/2022/day_10.ex
+++ lib/2022/day_10.ex
@@ -1,7 +1,36 @@
 defmodule AdventOfCode.Y2022.Day10 do
+  defp prepare_input(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.reduce([{1, 1}], fn
+      "noop", [{c, x} | _] = cycles ->
+        [{c + 1, x} | cycles]
+
+      "addx " <> v, [{c, x} | _] = cycles ->
+        v = String.to_integer(v)
+        [{c + 2, x + v}, {c + 1, x} | cycles]
+    end)
+    |> Enum.drop(1)
+  end
+
   def part1(args) do
+    args
+    |> prepare_input()
+    |> Enum.filter(fn {c, _} -> c in [20, 60, 100, 140, 180, 220] end)
+    |> Enum.map(&Tuple.product/1)
+    |> Enum.sum()
   end
 
   def part2(args) do
+    args
+    |> prepare_input()
+    |> Enum.reverse()
+    |> Enum.map(fn {c, x} ->
+      pos = rem(c - 1, 40)
+      if abs(pos - x) <= 1, do: "#", else: "."
+    end)
+    |> Enum.chunk_every(40)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.each(&IO.puts/1)
   end
 end
--- test/2022/day_10_test.exs
+++ test/2022/day_10_test.exs
@@ -3,19 +3,17 @@ defmodule AdventOfCode.Y2022.Day10Test do
 
   import AdventOfCode.Y2022.Day10
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = AdventOfCode.Input.get!(10, 2022)
     result = part1(input)
 
-    assert result
+    assert result == 12560
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(10, 2022)
     result = part2(input)
 
-    assert result
+    assert result == :ok
   end
 end
