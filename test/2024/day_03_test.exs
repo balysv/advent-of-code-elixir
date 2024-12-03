63d383a8cdddab87666e5884bd79dbd43b4665d7
balysv <balys@valentukevicius.com>
2024-12-03 08:08:50 +0100

    2024, Day 3 (AI)
    Part 1: 1 shot
    Part 2: many shot

--- test/2024/day_03_test.exs
+++ test/2024/day_03_test.exs
@@ -6,14 +6,16 @@ defmodule AdventOfCode.Y2024.Day03Test do
   test "part1" do
     input = AdventOfCode.Input.get!(3, 2024)
     result = part1(input)
-    # Update with actual result
-    assert result == nil
+    # 1 shot
+    assert result == 167_650_499
   end
 
   test "part2" do
     input = AdventOfCode.Input.get!(3, 2024)
     result = part2(input)
-    # Update with actual result
-    assert result == nil
+    # many many shots; missed a crucial bug that
+    # the enabled flag was not being passed between
+    # lines.
+    assert result == 95_846_796
   end
 end
