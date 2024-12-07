473658af346395d937e3cc60956ecabdaa8bf11c
balysv <balys@valentukevicius.com>
2024-12-07 14:43:59 +0100

    2024, Day 6 (AI)
    Part 1: many shots as it mixed up left and right and was very confident about it
    Part 2: 1 shot

--- test/2024/day_06_test.exs
+++ test/2024/day_06_test.exs
@@ -6,14 +6,14 @@ defmodule AdventOfCode.Y2024.Day06Test do
   test "part1" do
     input = AdventOfCode.Input.get!(6, 2024)
     result = part1(input)
-    # Update with actual result
-    assert result == nil
+    # Many shots as it mixed up what's left and right
+    assert result == 4939
   end
 
   test "part2" do
     input = AdventOfCode.Input.get!(6, 2024)
     result = part2(input)
-    # Update with actual result
-    assert result == nil
+    # 1 shot but rather slow
+    assert result == 1434
   end
 end
