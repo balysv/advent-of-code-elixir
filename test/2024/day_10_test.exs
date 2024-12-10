5a03a3d83b2f142856595847c6b2b7c344897e1c
balysv <balys@valentukevicius.com>
2024-12-10 08:37:40 +0100

    2024, Day 10 (AI)
    Part 1: Didn't complete as requirements of scoring weren't understood
    Part 2: Didn't complete for same reason even though it saw part 1 solution

--- test/2024/day_10_test.exs
+++ test/2024/day_10_test.exs
@@ -6,14 +6,16 @@ defmodule AdventOfCode.Y2024.Day10Test do
   test "part1" do
     input = AdventOfCode.Input.get!(10, 2024)
     result = part1(input)
-    # Update with actual result
-    assert result == nil
+    # Many shots, the AI didn't understand the requirements
+    # of computing the score of a trailhead.
+    assert result == 659
   end
 
   test "part2" do
     input = AdventOfCode.Input.get!(10, 2024)
     result = part2(input)
-    # Update with actual result
-    assert result == nil
+    # Many shots as again the AI didn't understand the requirements
+    # of computing the score of a trailhead.
+    assert result == 1463
   end
 end
